9af27488196753cc67603f393dbf1fa810def3ab
Yurii Koba <kobayurii88@gmail.com>
2024-08-02 16:09:11 +0300

    (rpc_server): fix transaction details borsh deserialize (#317)
    * fix transaction details borsh deserialize
    
    * clippy

--- rpc-server/src/modules/transactions/mod.rs
+++ rpc-server/src/modules/transactions/mod.rs
@@ -11,11 +11,7 @@ pub(crate) async fn try_get_transaction_details_by_hash(
     if let Ok(transaction_details_bytes) =
         &data.tx_details_storage.retrieve(&tx_hash.to_string()).await
     {
-        Ok(
-            borsh::from_slice::<readnode_primitives::TransactionDetails>(
-                transaction_details_bytes,
-            )?,
-        )
+        readnode_primitives::TransactionDetails::borsh_deserialize(transaction_details_bytes)
     } else if let Some(tx_cache_storage) = data.tx_cache_storage.clone() {
         Ok(tx_cache_storage.get_tx_by_tx_hash(tx_hash).await?)
     } else {
