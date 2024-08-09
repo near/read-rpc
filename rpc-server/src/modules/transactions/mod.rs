19b54658e5a1a99ee6f0f366b1fb5c93f736a42a
Yurii Koba <kobayurii88@gmail.com>
2024-08-09 12:27:12 +0300

    Temporary solution to receive transactions through an additional deserialization service

--- rpc-server/src/modules/transactions/mod.rs
+++ rpc-server/src/modules/transactions/mod.rs
@@ -3,14 +3,21 @@ use jsonrpc_v2::Data;
 
 pub mod methods;
 
+async fn fetch_tx_details(
+    tx_hash: &near_indexer_primitives::CryptoHash,
+) -> anyhow::Result<readnode_primitives::TransactionDetails> {
+    let data: serde_json::Value = reqwest::get(format!("http://localhost:9099/get-tx/{}", tx_hash.to_string())).await?.json().await?;
+    Ok(serde_json::from_value(data)?)
+}
+
 pub(crate) async fn try_get_transaction_details_by_hash(
     data: &Data<ServerContext>,
     tx_hash: &near_indexer_primitives::CryptoHash,
 ) -> anyhow::Result<readnode_primitives::TransactionDetails> {
     if let Ok(transaction_details_bytes) =
-        &data.tx_details_storage.retrieve(&tx_hash.to_string()).await
+        fetch_tx_details(tx_hash).await
     {
-        readnode_primitives::TransactionDetails::tx_deserialize(transaction_details_bytes)
+        Ok(transaction_details_bytes)
     } else if let Some(tx_cache_storage) = data.tx_cache_storage.clone() {
         Ok(tx_cache_storage.get_tx_by_tx_hash(tx_hash).await?)
     } else {
